af6341127a529face69b28d34e1fb506951899a0
Andrei <andrei.lashewitch@icloud.com>
2022-04-14 22:22:09 +0700

    Окончательно запутался с последним, хотя конструкция кажетс верной

--- dz2.5.py
+++ dz2.5.py
@@ -1,11 +1,15 @@
 from decimal import Decimal
 price_list = [44.50, 85.60, 12.40, 58.60, 78.05, 697.12, 68.78, 79.63, 8921.53, 9798.23]
-members = float(price_list)
+price_list1 = list(map(str, price_list))
+#price_list_str = ','.join(price_list)
+print(type(price_list1))
 
+rub, cop = map(float, price_list1.split())
+count = int(price_list1)
+cop += rub * 100
+answer = cop * count
+rub = answer//100
+cop = str(answer%100)
+if len(cop) == 1: cop = '0'+cop
 
-x = Decimal(price_list[0])
-a = int(x)
-b = int(100 * (x - a))
-print(a, b)
-a, b = map(int, price_list.split('.'))
-print(a, b)
+print(f' { rub } руб. { cop } коп.')
